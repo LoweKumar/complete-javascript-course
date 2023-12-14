a4b1d2c61045e549c9b86922a77c8928df5dec1c
Lowe Kumar <lowekr00@gmail.com>
2023-12-15 02:09:07 +0530

    Our First AJAX Call: XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,5 +2,38 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-console.log("Hello World from 16-Asynchronous")
+// console.log("HelloWorld")
 ///////////////////////////////////////
+
+// Our First AJAX Call: XMLHttpRequest
+const getCountryData = function(country){
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load', function(){
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        const html = `
+        <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.continents}</h4>
+            <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+    });
+};
+
+getCountryData('south africa');
+getCountryData('Australia');
+
+
